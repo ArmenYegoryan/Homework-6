c71eabcbf961f44e60cb6641833ef8afbe2642d0
Armen <armen.yegoryan97@gmail.com>
2022-02-22 18:38:46 +0400

    clearTimeout

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import moment from "moment";
 import fs from "fs";
 import { Transform, Readable, Writable } from "stream";
 
+let id = 0;
+
 const rs = new Readable({
     read() {
-        setTimeout(() => {
+        id = setTimeout(() => {
             this.push(moment().toISOString());
         }, 1000);
     },
@@ -14,13 +16,14 @@ const ws = new Writable({
     write(chunk, UTF8, next) {
         fs.writeFile("momentJS.txt", chunk, { flag: "a" }, next);
     }
-})
+});
 
 const ts = new Transform({
     transform(chunk, UTF8, callback) {
+        clearTimeout(id);
         this.push((moment(chunk).format("MMMM Do YYYY, h:mm:ss:SS a")).toString() + "\n");
         callback();
     }
-})
+});
 
 rs.pipe(ts).pipe(ws);
